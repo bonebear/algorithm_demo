978706e66d24b591701890e8dd32aeae7572ec75
bonebear <bonebear1024@gmail.com>
2020-05-04 18:13:25 +0800

    update

--- graph_BFS.py
+++ graph_BFS.py
@@ -25,30 +25,6 @@ def findF(people):
                 checked.append(people)
                 lianxi_queue += graph[people]
 
-def findF_2(people):
-    lianxi_queue = deque()
-    lianxi_queue += graph[people]
-    path = {}
-    path[people] = graph[people]
-    checked = []
-    count = 0
-    step = -1
-    while lianxi_queue:
-        people = lianxi_queue.popleft()
-        # path += people
-        if people not in checked:
-            if people == "F":
-                count += 1
-            else:
-                step += 1
-                checked.append(people)
-                lianxi_queue += graph[people]
-                path[people] = graph[people]
-    print "一共有", count, "条需要",step,"步的最短路径可以找到F"
-    print path["A"]
 
 
-
-
-#findF("A")
-findF_2("A")
+findF("A")
